a7d267b521117aa913491a7e94d3459b9a55f11c
Vishal Sharma <vishalimp03@gmail.com>
2026-02-26 23:47:46 +0530

    26-FEB-2026 Binary Search Added FindIndexOftarget.java that return first or last occurring index of target value based on boolean flag. Deafult is -1

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
 import arrays.ArraysOfVishal;
 import binarysearch.BinarySearch;
+import binarysearch.FindIndexOfTarget;
 import sorting.MergeSort;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] a = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        BinarySearch bs = new BinarySearch();
-        System.out.println(bs.find(a, 0, a.length - 1, 5, -1));
+        int[] a = new int[] { 1, 2, 3, 4, 7, 8, 9 };
+
+        FindIndexOfTarget obj = new FindIndexOfTarget();
+        System.out.println("First: " + obj.find(a, 0, a.length - 1, 5, true));
+        System.out.println("Last: " + obj.find(a, 0, a.length - 1, 5, false));
 
     }
 }
---
+++ binarysearch/FindIndexOfTarget.java
@@ -0,0 +1,45 @@
+package binarysearch;
+
+public class FindIndexOfTarget {
+
+    /*
+     * returns first or last occurrence of target in a sorted array using binary
+     * search
+     */
+
+    /**
+     * 
+     * @param nums   Integer array in which searching is executed
+     * @param s      Starting index of range in integer array
+     * @param e      Ending index of range in integer array
+     * @param target Value to search in integer array
+     * @param first  If true then finds first occurence of target, else finds last
+     *               occurrenec of target
+     * @return return valid index based on first or last occurrence, returns -1 if
+     *         target is not found
+     */
+    public int find(int[] nums, int s, int e, int target, boolean first) {
+        int idx = -1;
+        if (s > e)
+            return idx;
+
+        int mid;
+        while (s <= e) {
+            mid = s + (e - s) / 2;
+            if (nums[mid] == target) {
+                idx = mid;
+                if (first) {
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return idx;
+    }
+
+}
